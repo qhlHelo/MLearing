ce93124e4f3936b12eecb2d4e83459aa45d767db
qhlhelo <qhlhelo@163.com>
2020-07-22 23:53:46 +0800

    update homework

--- Kaikeba_ML/homework/Assignment02.py
+++ Kaikeba_ML/homework/Assignment02.py
@@ -40,52 +40,46 @@ X_ = (X_ - np.mean(X_, axis=0)) / np.std(X_, axis=0)
 """
     初始化网络参数: 定义隐藏层维度，w1,b1,w2,b2
 """
-print(X_)
+n_features = X_.shape[1]  # 特征的维度
+n_hidden = 10  # 隐藏层维度
+w1 = np.random.randn(n_features, n_hidden)  # 标准正太分布
+b1 = np.zeros(n_hidden)
+w2 = np.random.randn(n_hidden, 1)
+b2 = np.zeros(1)
 
-n_features = X_.shape[1]
-print(n_features)
-# n_hidden = 10
-# w1 = np.random.randn(n_features, n_hidden)
-# b1 = np.zeros(n_hidden)
-# w2 = np.random.randn(n_hidden, 1)
-# b2 = np.zeros(1)
-#
-#
-# # ReLU函数
-# def Relu(x):
-#     """ 这里写你的代码 """
-#     pass
-#
-#
-# # 设置学习率
-# learning_rate = 1e-6
-#
-#
-# # 定义损失函数
-# def MSE_loss(y, y_hat):
-#     """ 这里写你的代码 """
-#     pass
-#
-#
-# # 定义线性回归函数
-# def Linear(X, W1, b1):
-#     """ 这里写你的代码 """
-#     pass
-#
-#
-# # 5000次迭代
-# for t in range(5000):
-#     # 前向传播，计算预测值y (Linear->Relu->Linear)
-#     """ 这里写你的代码 """
-#
-#     # 计算损失函数, 并输出每次epoch的loss
-#     """ 这里写你的代码 """
-#
-#     # 反向传播，基于loss 计算w1和w2的梯度
-#     """ 这里写你的代码 """
-#
-#     # 更新权重, 对w1, w2, b1, b2进行更新
-#     """ 这里写你的代码 """
-#
-# # 得到最终的w1, w2
-# print('w1={} \n w2={}'.format(w1, w2))
+
+# ReLU函数
+def ReLU(x):
+    return np.maximum(0, x)
+
+
+# 设置学习率
+learning_rate = 1e-6
+
+
+# 定义损失函数
+def MSE_loss(y, y_hat):
+    return 1 / 2 * np.mean(np.square(y - y_hat))
+
+
+# 定义线性回归函数
+def Linear(x, W, b):
+    return x ** 2 + W * x + b
+
+
+# 5000次迭代
+for t in range(5000):
+    # 前向传播，计算预测值y (Linear->Relu->Linear)
+    """ 这里写你的代码 """
+
+    # 计算损失函数, 并输出每次epoch的loss
+    """ 这里写你的代码 """
+
+    # 反向传播，基于loss 计算w1和w2的梯度
+    """ 这里写你的代码 """
+
+    # 更新权重, 对w1, w2, b1, b2进行更新
+    """ 这里写你的代码 """
+
+# 得到最终的w1, w2
+print('w1={} \n w2={}'.format(w1, w2))
