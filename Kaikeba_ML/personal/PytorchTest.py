9ccbcba2ca09488b7e0d20529e5f8f8fe570e9f3
qinhailong <qhlhelo@163.com>
2020-08-12 19:14:02 +0800

    pytorch update

--- Kaikeba_ML/personal/PytorchTest.py
+++ Kaikeba_ML/personal/PytorchTest.py
@@ -41,3 +41,49 @@ squeeze：降维
 # print(e, "....", e.size(), e.dim())  # [2,1,3]
 # f = d.squeeze(0)
 # print(f, "....", f.size(), f.dim())  # [2,3],恢复到c
+
+'''
+pow：求幂
+'''
+# a = torch.Tensor([[1, 2, 3], [4, 5, 6]])
+# print(a.pow(-1))
+# print(a.pow(2))
+
+'''
+sum：求和
+'''
+# a = torch.Tensor([[1, 2, 3], [4, 5, 6]])
+# print(a.sum(dim=0))  # 按列求和
+# print(torch.sum(a, 1))  # 按行求和
+
+# print(a.diag_embed())  # 对角矩阵
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+'''
+对由多个输入平面组成的输入信号进行一维卷积
+'''
+# F.conv1d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1)
+'''
+对由多个输入平面组成的输入图像应用二维卷积
+'''
+# F.conv2d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1)
+'''
+激活函数
+'''
+# F.relu(input, inplace=False)
+'''
+在由多个输入平面组成的输入信号上应用1D 最大池化
+'''
+# F.max_pool1d(*args, **kwargs)
+'''
+在由多个输入平面组成的输入信号上应用2D 最大池化
+'''
+# F.max_pool2d(*args, **kwargs)
+'''
+在训练过程中, 使用伯努利分布的样本, 随机的用概率p将输入张量的元素归零p，丢掉概率， 默认值为0.5
+training，如果 True 使用 dropout，默认为True
+inplace，如果设置为 True, 将会原地操作，默认为False
+'''
+# F.dropout(input, p=0.5, training=True, inplace=False)
